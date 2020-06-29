6b1be672afa49d0c53a31550db1154f754a845ce
OLAF74 <maxxa01@mail.ru>
2020-06-29 13:09:37 +0500

    added multiselectable answers

--- SusuTester/Json/Models/QuestionModel.cs
+++ SusuTester/Json/Models/QuestionModel.cs
@@ -13,11 +13,20 @@ namespace SusuTester.Json.Models
         public string QuestionImg { get; set; }
 
 
+        [JsonProperty(PropertyName = "is_multi_select")]
+        public bool IsMultiSelect { get; set; }
+
+
         [JsonProperty(PropertyName = "answers")]
         public List<AnswerModel> Answers { get; set; }
 
 
         [JsonProperty(PropertyName = "right_answer_index")]
         public int RightAnswerIndex { get; set; }
+
+        [JsonProperty(PropertyName = "right_answer_index_array")]
+        public int[] RightAnswerIndexArray { get; set; }
+
+
     }
 }
--- SusuTester/Views/QuestionScreen.cs
+++ SusuTester/Views/QuestionScreen.cs
@@ -3,6 +3,8 @@ using System.Windows.Forms;
 using SusuTester.Json.Models;
 using System.IO;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SusuTester
 {
@@ -24,54 +26,102 @@ namespace SusuTester
             if (!string.IsNullOrEmpty(QuestionInstance.Question))
                 QuestionText.Text = QuestionInstance.Question;
 
-           if (!string.IsNullOrEmpty(QuestionInstance.QuestionImg) && File.Exists(QuestionInstance.QuestionImg))
+            if (!string.IsNullOrEmpty(QuestionInstance.QuestionImg) && File.Exists(QuestionInstance.QuestionImg))
                 QuestionImage.Image = Image.FromFile(QuestionInstance.QuestionImg);
 
 
             foreach (AnswerModel answer in QuestionInstance.Answers)
             {
-                RadioButton radioButton = new RadioButton();
-                radioButton.ImageAlign = ContentAlignment.MiddleLeft;
-                radioButton.TextImageRelation = TextImageRelation.ImageBeforeText;
-                
-              
-                
-                if (!string.IsNullOrEmpty(answer.Answer))
-                    radioButton.Text = answer.Answer;
+                if (!QuestionInstance.IsMultiSelect)
+                {
+                    RadioButton radioButton = new RadioButton();
+                    radioButton.ImageAlign = ContentAlignment.MiddleLeft;
+                    radioButton.TextImageRelation = TextImageRelation.ImageBeforeText;
 
-                if (!string.IsNullOrEmpty(answer.AnswerImg) && File.Exists(answer.AnswerImg)) {
-                    Image image = Image.FromFile(answer.AnswerImg);
 
-                    float scaleHeight = 200 / (float)image.Height;
-                    float scaleWidth = 200 / (float)image.Width;
-                    float scale = Math.Min(scaleHeight, scaleWidth);
 
-                    Bitmap bmp = new Bitmap(image, (int)(image.Width * scale), (int)(image.Height * scale));
-                    radioButton.Image = bmp;
-                  
-                    image.Dispose();
+                    if (!string.IsNullOrEmpty(answer.Answer))
+                        radioButton.Text = answer.Answer;
+
+                    if (!string.IsNullOrEmpty(answer.AnswerImg) && File.Exists(answer.AnswerImg))
+                    {
+                        Image image = Image.FromFile(answer.AnswerImg);
+
+                        float scaleHeight = 200 / (float)image.Height;
+                        float scaleWidth = 200 / (float)image.Width;
+                        float scale = Math.Min(scaleHeight, scaleWidth);
+
+                        Bitmap bmp = new Bitmap(image, (int)(image.Width * scale), (int)(image.Height * scale));
+                        radioButton.Image = bmp;
+
+                        image.Dispose();
+                    }
+                    radioButton.MaximumSize = new Size(0, 250);
+                    radioButton.AutoSize = true;
+
+                    AnswersGroup.Controls.Add(radioButton);
                 }
-                radioButton.MaximumSize = new Size(0, 250);
-                radioButton.AutoSize = true;
+                else {
+                    CheckBox checkBox = new CheckBox();
+                    checkBox.ImageAlign = ContentAlignment.MiddleLeft;
+                    checkBox.TextImageRelation = TextImageRelation.ImageBeforeText;
 
-                AnswersGroup.Controls.Add(radioButton);
+                    if (!string.IsNullOrEmpty(answer.Answer))
+                        checkBox.Text = answer.Answer;
+
+                    if (!string.IsNullOrEmpty(answer.AnswerImg) && File.Exists(answer.AnswerImg))
+                    {
+                        Image image = Image.FromFile(answer.AnswerImg);
+
+                        float scaleHeight = 200 / (float)image.Height;
+                        float scaleWidth = 200 / (float)image.Width;
+                        float scale = Math.Min(scaleHeight, scaleWidth);
+
+                        Bitmap bmp = new Bitmap(image, (int)(image.Width * scale), (int)(image.Height * scale));
+                        checkBox.Image = bmp;
+
+                        image.Dispose();
+                    }
+                    checkBox.MaximumSize = new Size(0, 250);
+                    checkBox.AutoSize = true;
+
+                    AnswersGroup.Controls.Add(checkBox);
+                }
+
+
+                
             }
         }
 
         public bool IsRightAnswered()
         {
-            int answerIndex = -1;
-            for (int i = 0; i < AnswersGroup.Controls.Count; i++)
+            if (!QuestionInstance.IsMultiSelect)
             {
-                RadioButton radioButton = (RadioButton)AnswersGroup.Controls[i];
-                if (radioButton.Checked)
+                int answerIndex = -1;
+                for (int i = 0; i < AnswersGroup.Controls.Count; i++)
                 {
-                    answerIndex = i;
-                    break;
+                    RadioButton radioButton = (RadioButton)AnswersGroup.Controls[i];
+                    if (radioButton.Checked)
+                    {
+                        answerIndex = i;
+                        break;
+                    }
                 }
-            }
 
-            return QuestionInstance.RightAnswerIndex == answerIndex;
+                return QuestionInstance.RightAnswerIndex == answerIndex;
+            }
+            else
+            {
+                List<int> AnswerIndexArray = new List<int>();
+                for (int i = 0; i < AnswersGroup.Controls.Count; i++)
+                {
+                    CheckBox radioButton = (CheckBox)AnswersGroup.Controls[i];
+                    if (radioButton.Checked)
+                        AnswerIndexArray.Add(i);
+                }
+
+                return Enumerable.SequenceEqual(AnswerIndexArray, QuestionInstance.RightAnswerIndexArray);
+            }
         }
     }
 }
